0446aa6a9dd7eb591173d9e9a34156a12ed2eb3a
kevinmillo <kbullor@hotmail.com>
2022-01-27 23:13:57 +0000

    creacion navbar + carga logos en img

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../../img/Logo-con-nombre.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-white pe-none">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src={Logo}
+            alt="xmile"
+            width="80"
+            height="50"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a
+                class="nav-link active pe-auto text-info"
+                aria-current="page"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active pe-auto" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active pe-auto" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <button
+                type="button"
+                class="btn btn-success pe-auto position-absolute end-0 pr-2"
+              >
+                Cerrar Sesión
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
