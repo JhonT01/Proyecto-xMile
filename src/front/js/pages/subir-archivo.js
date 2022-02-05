122079d2daf58c177d496281ea50d3fcfc9210ea
JhonT01 <jftc_94@hotmail.com>
2022-02-05 21:53:38 +0000

    Se mantiene el error pero se redujo el codigo basura generado para las pruebas

--- src/front/js/pages/subir-archivo.js
+++ src/front/js/pages/subir-archivo.js
@@ -4,7 +4,6 @@ import { useDropzone } from "react-dropzone";
 import logoIma from "../../img/Prototipo3.png";
 import "../../styles/home.css";
 
-// Se importa para probar el funcionamiento del Dropzone en la pagina principal, borrar e importar donde sea necesario
 import { Dropzone } from "../component/dropzone-component";
 
 export const Subir_archivo = () => {
@@ -20,7 +19,6 @@ export const Subir_archivo = () => {
   const enviarArchivo = (data) => {
     data.forEach(function (element) {
       const formData = new FormData();
-
       formData.append("file", element);
 
       fetch(
@@ -38,25 +36,6 @@ export const Subir_archivo = () => {
           console.error("Error:", error);
         });
     });
-  };
-
-  const uploadFiles = async (data) => {
-    console.log(data);
-    try {
-      let response = await fetch(
-        "https://3001-jhont01-proyectoxmile-olnndlpoqt9.ws-us30.gitpod.io/upload",
-        {
-          method: "POST",
-          body: data,
-        }
-      );
-      console.log(response.ok); // will be true if the response is successfull
-      console.log(response.status); // the status code = 200 or code = 400 etc.
-      let responseObject = await response.json();
-      setTask(responseObject);
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
