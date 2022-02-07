fa3f02771c862d7d0cdda89a22d527e5eef3c7c7
JhonT01 <jftc_94@hotmail.com>
2022-02-07 00:29:38 +0000

    Landing page esqueleto, pendiente actualizar informacion

---
+++ src/front/js/pages/landingpage.js
@@ -0,0 +1,301 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/landingpage.css";
+
+export const Landing_page = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div>
+      {/* <!--     HERO CONTAINER     --> */}
+      <div className="container my-5">
+        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+          <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 className="display-4 fw-bold lh-1">
+              Border hero with cropped image and shadows
+            </h1>
+            <p className="lead">
+              Quickly design and customize responsive mobile-first sites with
+              Bootstrap, the worlds most popular front-end open source toolkit,
+              featuring Sass variables and mixins, responsive grid system,
+              extensive prebuilt components, and powerful JavaScript plugins.
+            </p>
+            {/* <div className="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg px-4 me-md-2 fw-bold"
+              >
+                Primary
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4"
+              >
+                Default
+              </button>
+            </div> */}
+          </div>
+          <div className="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+            <img
+              className="rounded-lg-3"
+              src="bootstrap-docs.png"
+              alt=""
+              width="720"
+            />
+          </div>
+        </div>
+      </div>
+      {/* <!--     /HERO CONTAINER     --> */}
+
+      <div className="b-example-divider"></div>
+
+      {/* <!--     FEATURE 1     --> */}
+
+      <div className="container px-4 py-5" id="featured-3">
+        <h2 className="pb-2 border-bottom">Columns with icons</h2>
+        <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div className="feature col">
+            <div className="feature-icon bg-primary bg-gradient">
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </div>
+            <h2>Featured title</h2>
+            <p>
+              Paragraph of text beneath the heading to explain the heading.
+              We'll add onto it with another sentence and probably just keep
+              going until we run out of words.
+            </p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </a>
+          </div>
+          <div className="feature col">
+            <div className="feature-icon bg-primary bg-gradient">
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </div>
+            <h2>Featured title</h2>
+            <p>
+              Paragraph of text beneath the heading to explain the heading.
+              We'll add onto it with another sentence and probably just keep
+              going until we run out of words.
+            </p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </a>
+          </div>
+          <div className="feature col">
+            <div className="feature-icon bg-primary bg-gradient">
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </div>
+            <h2>Featured title</h2>
+            <p>
+              Paragraph of text beneath the heading to explain the heading.
+              We'll add onto it with another sentence and probably just keep
+              going until we run out of words.
+            </p>
+            <a href="#" className="icon-link">
+              Call to action
+              <svg className="bi" width="1em" height="1em">
+                <use href=""></use>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* <!--    /FEATURE 1     --> */}
+
+      <div className="b-example-divider"></div>
+
+      {/* <!--     FEATURE 2     --> */}
+
+      <div className="container px-4 py-5" id="custom-cards">
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+        />
+
+        <div className="container py-5">
+          <div className="row mb-4">
+            <div className="col-lg-5">
+              <h2 className="display-4 font-weight-light">Our team</h2>
+              <p className="font-italic text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+
+          <div className="row text-center align-items-center">
+            {/* <!-- Team item--> */}
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Manuella Nevoresky</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- End--> */}
+
+            {/* <!-- Team item--> */}
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Samuel Hardy</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- End--> */}
+
+            {/* <!-- Team item--> */}
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar8.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">Tom Sunderland</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- End--> */}
+
+            {/* <!-- Team item--> */}
+            <div className="col-xl-3 col-sm-6 mb-5">
+              <div className="bg-white rounded shadow-sm py-5 px-4">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar2.png"
+                  alt=""
+                  width="100"
+                  className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"
+                />
+                <h5 className="mb-0">John Tarly</h5>
+                <span className="small text-uppercase text-muted">
+                  CEO - Founder
+                </span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-facebook-f"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#" className="social-link">
+                      <i className="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            {/* <!-- End--> */}
+          </div>
+        </div>
+      </div>
+      {/* <!--    /FEATURE 2     --> */}
+    </div>
+  );
+};
