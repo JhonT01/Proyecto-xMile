9c91e6a20e44dc04cc1bd98713b4169e7a1b79b9
JhonT01 <jftc_94@hotmail.com>
2022-02-15 00:54:44 +0000

    Botones y capacidad de enrutamiento del home solucionado junto a botones de inicio o de log out incorporado, falta leer toquen de sesion

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,68 +1,106 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../../img/Logo-con-nombre.png";
 import "../../styles/navbar.css";
 
+const IsLogged = () => {
+  return (
+    <>
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" aria-current="page" href="#">
+          Profile
+        </a>
+      </li>
+      x
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" href="/principal/1">
+          Clientes
+        </a>
+      </li>
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" href="#">
+          Crear Cliente
+        </a>
+      </li>
+      <li className="nav-item sombreado">
+        <Link className="nav-link active pe-auto" to="/subir-archivo/1">
+          Cargar Facturas
+        </Link>
+      </li>
+      <li className="nav-item">
+        <button
+          type="button"
+          className="btn pe-auto position-absolute end-0 pr-2"
+        >
+          Cerrar Sesión
+        </button>
+      </li>
+    </>
+  );
+};
+
+const IsNotLogged = () => {
+  return (
+    <>
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" aria-current="page" href="#">
+          Profile
+        </a>
+      </li>
+
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" href="/principal/1">
+          Clientes
+        </a>
+      </li>
+
+      <li className="nav-item sombreado">
+        <a className="nav-link active pe-auto" href="#">
+          Crear Cliente
+        </a>
+      </li>
+
+      <li className="nav-item sombreado">
+        <Link className="nav-link active pe-auto" to="/subir-archivo/1">
+          Cargar Facturas
+        </Link>
+      </li>
+
+      <li className="nav-item  position-absolute end-0 pr-2">
+        <button type="button" className="btn pe-auto">
+          Registrarse
+        </button>
+
+        <button type="button" className="btn pe-auto">
+          Iniciar Sesión
+        </button>
+      </li>
+    </>
+  );
+};
+
 export const Navbar = () => {
-  const { store, actions } = useContext(Context);
+  // const { store, actions } = useContext(Context);
 
-  const params = useParams();
+  // const params = useParams();
 
-  const estatus = true;
-
-  const is_logged = (estatus) => {
-    if (estatus == true) {
-      return (
-        <>
-          <li className="nav-item sombreado">
-            <a className="nav-link active pe-auto" aria-current="page" href="#">
-              Profile
-            </a>
-          </li>
-          <li className="nav-item sombreado">
-            <a className="nav-link active pe-auto" href="/principal/1">
-              Clientes
-            </a>
-          </li>
-          <li className="nav-item sombreado">
-            <a className="nav-link active pe-auto" href="#">
-              Crear Cliente
-            </a>
-          </li>
-          <li className="nav-item sombreado">
-            <Link className="nav-link active pe-auto" to="/subir-archivo/1">
-              Cargar Facturas
-            </Link>
-          </li>
-          <li className="nav-item">
-            <button
-              type="button"
-              className="btn pe-auto position-absolute end-0 pr-2"
-            >
-              Cerrar Sesión
-            </button>
-          </li>
-        </>
-      );
-    } else {
-      return <></>;
-    }
-  };
+  const [status, setStatus] = useState(false);
 
   return (
-    <nav className="navbar-fixed navbar-expand-lg navbar-light bg-white pe-none">
+    <nav className="navbar-fixed navbar-expand-lg navbar-light bg-white">
       <div className="container-fluid p-0 mt-n1 d-flex">
-        {/* <a className="navbar-brand" href="#"> */}
         <Link to="/" className="navbar-brand">
-          <img
-            src={Logo}
-            alt="xmile"
-            width="80"
-            height="50"
-            className="d-inline-block align-text-top"
-          />
+          <a className="navbar-brand" href="/">
+            <img
+              src={Logo}
+              alt="xmile"
+              width="50"
+              height="50"
+              className="d-inline-block align-text-top"
+            />
+          </a>
         </Link>
-        {/* </a> */}
+
         <button
           className="navbar-toggler"
           type="button"
@@ -74,11 +112,10 @@ export const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mt-n1 p-0">
-            {(estatus) => {
-              is_logged(estatus);
-            }}
+            {status ? <IsLogged /> : <IsNotLogged />}
           </ul>
         </div>
       </div>
